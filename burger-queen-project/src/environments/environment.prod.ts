e34638abee45ffdc113d798444495533430bdf50
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-04-22 20:01:55 -0500

    Se añadiò un valor booleano:true al production de environment sugerido por el ERROR tras hacer el build

--- burger-queen-project/src/environments/environment.prod.ts
+++ burger-queen-project/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9t3y5npbAvPf0BA5w9o6M47z6aLuMnMo",
+    authDomain: "burger-queen-angular.firebaseapp.com",
+    databaseURL: "https://burger-queen-angular.firebaseio.com",
+    projectId: "burger-queen-angular",
+    storageBucket: "burger-queen-angular.appspot.com",
+    messagingSenderId: "543416604480"
+  }
   
 };
